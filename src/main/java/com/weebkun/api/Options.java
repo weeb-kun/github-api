35454a959d278dc902236d6bc06c392006bab839
Weebkun <2002umpa@gmail.com>
2020-10-11 15:19:55 +0800

    moved OrgBuilder to Organisation. added javadoc to Options.

--- src/main/java/com/weebkun/api/Options.java
+++ src/main/java/com/weebkun/api/Options.java
@@ -12,24 +12,54 @@ import com.weebkun.utils.ParamConflictException;
  * </p>
  */
 public class Options {
+    /**
+     * can be 'all', 'public' or 'private'.
+     */
     public String visibility;
+    /**
+     * can be 'owner', 'collaborator' or 'organization_member'.
+     */
     public String affiliation;
 
     /**
+     * can be 'all', 'owner', 'public', 'private', or 'member'.
+     *
      * cannot be used with visibility or affiliation.
      * will throw a {@link ParamConflictException} when used with visibility or affiliation.
      */
     public String type;
 
-    public String sort;
-    public String direction;
-    public int perPage;
-    public int page;
     /**
+     * sorting options.
+     * can be 'created', 'updated', 'pushed' or 'full_name'.
+     */
+    public String sort;
+
+    /**
+     * the direction of sorting.
+     * can be 'asc' or 'desc'.
+     */
+    public String direction;
+
+    /**
+     * the number of results per page.
+     * max is 100.
+     */
+    public int perPage;
+
+    /**
+     * current page to get.
+     */
+    public int page;
+
+    /**
+     * ISO 8601 timestamp indicating only to show updates after this time.
      * cannot be used with before.
      */
     public String since;
+
     /**
+     * ISO 8601 timestamp indicating only to show updates before this time.
      * cannot be used with since.
      */
     public String before;
--- src/main/java/com/weebkun/api/Organisation.java
+++ src/main/java/com/weebkun/api/Organisation.java
@@ -1,8 +1,13 @@
 package com.weebkun.api;
 
+import com.weebkun.utils.HttpErrorException;
+import com.weebkun.utils.UnauthorisedException;
+import okhttp3.MediaType;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
+import javax.print.attribute.standard.PresentationDirection;
 import java.io.IOException;
 
 /**
@@ -43,6 +48,49 @@ public class Organisation {
      */
     public Repository[] getRepos(Options params) {
         return Organisation.getRepos(this.name, params);
+    }
+
+    /**
+     * creates a repository in a specified organisation.
+     * the authenticated user must have access to this organisation and be able to create repositories in that organisation.
+     *
+     * use {@link OrgBuilder} instead if you dont want to specify the json yourself.
+     * @param org the name of the organisation
+     * @param json the json string
+     * @throws HttpErrorException if the user cannot create repositories in the specified organisation or another error occurred
+     * @see OrgBuilder
+     */
+    public static void createRepo(String org, String json) throws HttpErrorException {
+        RequestBody body = RequestBody.create(json, MediaType.get(Github.gsonMediaType));
+        Request request = new Request.Builder()
+                .url(Github.getRoot() + String.format("/orgs/%s/repos", org))
+                .post(body)
+                .build();
+        try(Response response = Github.getClient().newCall(request).execute()) {
+            if(response.code() != 201) throw new HttpErrorException(response);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * creates a repository in this organisation.
+     * use {@link OrgBuilder} if you do not want to specify the json yourself.
+     * @param json the json string
+     * @throws HttpErrorException if an error was received
+     * @see OrgBuilder
+     */
+    public void createRepo(String json) throws HttpErrorException{
+        RequestBody body = RequestBody.create(json, MediaType.get(Github.gsonMediaType));
+        Request request = new Request.Builder()
+                .url(Github.getRoot() + String.format("/orgs/%s/repos", this.name))
+                .post(body)
+                .build();
+        try(Response response = Github.getClient().newCall(request).execute()) {
+            if(response.code() != 201) throw new HttpErrorException(response);
+        } catch (IOException e) {
+             e.printStackTrace();
+        }
     }
 
     public String login;
@@ -90,4 +138,67 @@ public class Organisation {
     public boolean members_can_create_private_repositories;
     public boolean members_can_create_internal_repositories;
     public boolean members_can_create_pages;
+
+    /**
+     * meant for creating a repository in an organisation. the authenticated user must be a member of that organisation.
+     * see <a href="https://docs.github.com/en/free-pro-team@latest/rest/reference/repos#create-an-organization-repository">the github docs</a> for more info
+     */
+    public static final class OrgBuilder extends Repository.Builder {
+        // post /orgs/{org}/repos
+
+        /**
+         * the organisation name.
+         */
+        public String org;
+
+        /**
+         * the team id that will be granted access to this repo.
+         */
+        public int teamId;
+
+        /**
+         * creates an organisation repository. authenticated user must be part of that organisation.
+         * @throws HttpErrorException if user cannot create repositories in this organisation or another error cause the create operation to fail.
+         */
+        @Override
+        public void build() throws HttpErrorException{
+            String json = String.format("{" +
+                            "'name':'%s'" +
+                            "'description': '%s'" +
+                            "'homepage': '%s'" +
+                            "'private': '%s'" +
+                            "'visibility': '%s'" +
+                            "'has_issues': '%s'" +
+                            "'has_projects': '%s'" +
+                            "'has_wiki': '%s'" +
+                            "'is_template': '%s'" +
+                            "'team_id': '%s'" +
+                            "'auto_init': '%s'" +
+                            "'gitignore_template': '%s" +
+                            "'license_template': '%s'" +
+                            "'allow_squash_merge': '%s'" +
+                            "'allow_merge_commit': '%s'" +
+                            "'allow_rebase_merge': '%s'" +
+                            "'delete_branch_on_merge': '%s'" +
+                            "}",
+                    name,
+                    description,
+                    homepage,
+                    isPrivate,
+                    visibility,
+                    hasIssues,
+                    hasProjects,
+                    hasWiki,
+                    isTemplate,
+                    teamId,
+                    autoInit,
+                    gitignoreTemplate,
+                    licenseTemplate,
+                    allowSquashMerge,
+                    allowMergeCommit,
+                    allowRebaseMerge,
+                    deleteBranchOnMerge);
+            Organisation.createRepo(this.org, json);
+        }
+    }
 }
