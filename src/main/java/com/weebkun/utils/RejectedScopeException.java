6800f2cfabe0533ddcfabe0a191f59bfcb2b3292
Weebkun <2002umpa@gmail.com>
2020-10-18 16:42:46 +0800

    bump version

--- src/main/java/com/weebkun/utils/RejectedScopeException.java
+++ src/main/java/com/weebkun/utils/RejectedScopeException.java
@@ -1,4 +1,23 @@
 package com.weebkun.utils;
 
-public class RejectedScopeException {
+/**
+ * thrown when requested scopes are rejected by the user.
+ */
+public class RejectedScopeException extends RuntimeException {
+
+    private final String[] scopes;
+
+    /**
+     * throw a new {@code RejectedScopeException} with the scopes rejected as variable arguments.
+     * @param scopes the scopes that are rejected.
+     */
+    public RejectedScopeException(String... scopes) {
+        super("rejected scopes:\n" + String.join("\n", scopes));
+        this.scopes = scopes;
+    }
+
+    public RejectedScopeException(String message, String... scopes) {
+        super(message);
+        this.scopes = scopes;
+    }
 }
--- src/main/java/com/weebkun/utils/ScopeRevokedException.java
+++ src/main/java/com/weebkun/utils/ScopeRevokedException.java
@@ -1,4 +1,23 @@
 package com.weebkun.utils;
 
-public class ScopeRevokedException {
+/**
+ * thrown when a scope originally requested and granted have since been rejected by the user.
+ */
+public class ScopeRevokedException extends RuntimeException{
+
+    private final String[] scopes;
+
+    /**
+     * throw a new {@code ScopeRevokedException} with the scopes that were revoked by the user as variable arguments.
+     * @param scopes the scopes
+     */
+    public ScopeRevokedException(String... scopes) {
+        super("scopes rejected:\n" + String.join("\n", scopes));
+        this.scopes = scopes;
+    }
+
+    public ScopeRevokedException(String message, String... scopes) {
+        super(message);
+        this.scopes = scopes;
+    }
 }
